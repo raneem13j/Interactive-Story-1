9774e3152b8de5fd0d6220a2bc264f35793c86f6
Omar Khankan <omar.khankan@gmail.com>
2022-12-13 00:12:15 +0000

    Saving some changes

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,26 @@
 var img = document.getElementById("img");
-var txt=document.getElementById("txt");
-var txt1=document.getElementById("txt1");
+var txt = document.getElementById("txt");
+var txt1 = document.getElementById("txt1");
 
-
-
-
-function show(){
-    img.style='display:block';
-    txt.style='display:none';
-    txt1.style='display:block';
-
-
+function show() {
+  img.style = "display:block";
+  txt.style = "display:none";
+  txt1.style = "display:block";
 }
-function redirect(){
-    location.href="./html 6.html";
+function redirect() {
+  location.href = "./html 6.html";
 }
 
-function redirect1(){
-    location.href="./html 7.html";
+function redirect1() {
+  location.href = "./html 7.html";
 }
-function redirect2(){
-    location.href="./html 8.html";
+function redirect2() {
+  location.href = "./html 8.html";
 }
 
-function restart(){
-    var yes= confirm("do you want to restart");
-if(yes){
-location.href="./html 0.html";
-}
+function restart() {
+  var yes = confirm("do you want to restart");
+  if (yes) {
+    location.href = "./html 0.html";
+  }
 }
