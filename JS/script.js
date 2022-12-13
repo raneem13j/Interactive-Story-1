ce20938af304c4b2d95413be3e1275c42620c734
Omar Khankan <omar.khankan@gmail.com>
2022-12-13 00:08:36 +0000

    Adding HTML CSS JS

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+var img = document.getElementById("img");
+var txt=document.getElementById("txt");
+var txt1=document.getElementById("txt1");
+
+
+
+
+function show(){
+    img.style='display:block';
+    txt.style='display:none';
+    txt1.style='display:block';
+
+
+}
+function redirect(){
+    location.href="./html 6.html";
+}
+
+function redirect1(){
+    location.href="./html 7.html";
+}
+function redirect2(){
+    location.href="./html 8.html";
+}
+
+function restart(){
+    var yes= confirm("do you want to restart");
+if(yes){
+location.href="./html 0.html";
+}
+}
